ef94f181276cb59ce91fa7d84bfc8f268079b9cf
prateek <prateekkumarsmail@gmail.com>
2024-09-01 18:54:10 +0530

    contacts' obj & map() added

---
+++ contact_card_react/src/contact.js
@@ -0,0 +1,53 @@
+let contact = [
+    {
+      id: "0",
+      name: "Mcgregor",
+      tel: "9315******",
+      rel: "classmate",
+      img: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRrTRnA7CQVEN3ARkFRAP37fGIU5IYMeKJtU1VsMlAbMCDjdraw",
+    },
+    {
+      id: "1",
+      name: "Mavidal",
+      tel: "9315******",
+      rel: "classmate",
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkcGKiJ6gIaLL3KxJhBCp83FlMcC-3V6QjIw&s",
+    },
+    {
+      id: "2",
+      name: "GGG",
+      tel: "9315******",
+      rel: "classmate",
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzznX5b-RBu9EWbnnXJAnA-CUnO-vgrsLGKA&s",
+    },
+    {
+      id: "3",
+      name: "TATE",
+      tel: "9315******",
+      rel: "classmate",
+      img: "https://static.ffx.io/images/$zoom_2.3309%2C$multiply_0.5855%2C$ratio_1%2C$width_1059%2C$x_678%2C$y_162/t_crop_custom/q_86%2Cf_auto/89dcd9ed59d16f7c77f001daa35c38e30b16c8dc",
+    },
+    {
+      id: "4",
+      name: "Mr. Wick",
+      tel: "9315******",
+      rel: "classmate",
+      img: "https://ichef.bbci.co.uk/news/480/cpsprodpb/A4B9/production/_129096124_gettyimages-1474943797.jpg.webp",
+    },
+    {
+      id: "5",
+      name: "Mr. wayne",
+      tel: "9315******",
+      rel: "classmate",
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTugMOgi1CBP8KfIse9NgLGmEQvKUplBYmpZQ&s",
+    },
+    {
+      id: "6",
+      name: "Logan",
+      tel: "9315******",
+      rel: "classmate",
+      img: "https://i0.wp.com/cromossomonerd.com.br/wp-content/uploads/2017/12/logan-wallpaper-61659-63490-hd-wallpapers.jpg?fit=1920%2C1080&ssl=1",
+    },
+  ];
+  
+  export default contact;
